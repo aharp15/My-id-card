f1810b8ad22465192f2e115dbbd660a39d4f2a6d
aharp15 <52418119+aharp15@users.noreply.github.com>
2021-02-27 13:35:10 -0500

    Add files via upload

---
+++ My Profile.js
@@ -0,0 +1,37 @@
+function $(id){
+	return document.getElementById(id);
+}
+
+window.onload = function(){
+	
+	
+	//Whenever we click on one of our dots
+	//we want to change the background color of our card
+	
+	//1st we need to access the correct HTML elementFromPoint
+	//Then we add an event listener to determine when its been clicked on
+	$("red").addEventListener("click", function(){
+		
+		//Next we need to access the HTML element of our card
+		$("card").style.backgroundColor = "red";
+	});
+	
+	//Now we're gonna do the same thing with the rest of the dots
+	$("yellow").addEventListener("click", function(){
+		
+		//Next we need to access the HTML element of our card
+		$("card").style.backgroundColor = "yellow";
+	});
+	
+	$("blue").addEventListener("click", function(){
+		
+		//Next we need to access the HTML element of our card
+		$("card").style.backgroundColor = "blue";
+	});
+	
+	$("green").addEventListener("click", function(){
+		
+		//Next we need to access the HTML element of our card
+		$("card").style.backgroundColor = "green";
+	});
+}
